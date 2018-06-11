4bd76ac7d8e5d07acd8c969230084330c36669a6
Soulcramer <scott.rayapoulle@lunabee.com>
2018-06-11 15:37:11 +0200

    save before android x

---
+++ app/src/main/java/app/soulcramer/soone/ui/user/edit/EditUserFragment.kt
@@ -0,0 +1,56 @@
+package app.soulcramer.soone.ui.user.edit
+
+import android.arch.lifecycle.ViewModelProvider
+import android.arch.lifecycle.ViewModelProviders
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import app.soulcramer.soone.common.observeK
+import app.soulcramer.soone.di.Injectable
+import app.soulcramer.soone.ui.user.UserViewModel
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.fragment_user.*
+import org.threeten.bp.DateTimeUtils
+import org.threeten.bp.LocalDate
+import org.threeten.bp.Period
+import org.threeten.bp.ZoneId
+import javax.inject.Inject
+
+class EditUserFragment : Fragment(), Injectable {
+    @Inject
+    lateinit var viewModelFactory: ViewModelProvider.Factory
+
+    private lateinit var userViewModel: UserViewModel
+
+    override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_user_detail, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        userViewModel = ViewModelProviders.of(this, viewModelFactory)
+                .get(UserViewModel::class.java)
+
+        userViewModel.setId("1")
+        userViewModel.user.observeK(this) { userResource ->
+            userResource.data?.run {
+                nickNameTextView.text = nickName
+                descriptionTextView.text = description
+
+                val birthday = DateTimeUtils.toInstant(birthdate).atZone(ZoneId.systemDefault()).toLocalDate()
+                val now = LocalDate.now()
+                ageTextView.text = Period.between(birthday, now).years.toString()
+                Picasso.get()
+                        .load("https://media.notify.moe/images/covers/large/$id.jpg")
+                        .error(R.drawable.ic_image_off_black_24dp)
+                        .into(coverImageView)
+            }
+        }
+    }
+}
